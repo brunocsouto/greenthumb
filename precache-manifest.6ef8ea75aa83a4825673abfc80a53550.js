47ddd0f859ad07ec395dd3be838d2359c2098e1b
Bruno Souto <bruno.csouto@gmail.com>
2019-12-02 12:39:52 -0300

    Updates

---
+++ precache-manifest.6ef8ea75aa83a4825673abfc80a53550.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427cbed0e4ef43bf2cbf2db49a343db7",
+    "url": "/greenthumb/index.html"
+  },
+  {
+    "revision": "601876c63a5af0ba5ea5",
+    "url": "/greenthumb/static/css/main.f026c47d.chunk.css"
+  },
+  {
+    "revision": "1e79137785bf38afbcf4",
+    "url": "/greenthumb/static/js/2.ee8df2f8.chunk.js"
+  },
+  {
+    "revision": "601876c63a5af0ba5ea5",
+    "url": "/greenthumb/static/js/main.592db255.chunk.js"
+  },
+  {
+    "revision": "9d784ce384743b5bd1cc",
+    "url": "/greenthumb/static/js/runtime-main.daa4d83f.js"
+  },
+  {
+    "revision": "2783f801112d144bc540559ffdff84cd",
+    "url": "/greenthumb/static/media/arrow.2783f801.svg"
+  },
+  {
+    "revision": "7de066f385f7194a6069d989b275c1aa",
+    "url": "/greenthumb/static/media/arrow.7de066f3.svg"
+  },
+  {
+    "revision": "a1cc00d93b0cdd0cec31da3dffeaf603",
+    "url": "/greenthumb/static/media/illustration-home-mobile.a1cc00d9.png"
+  },
+  {
+    "revision": "b2c10a673f37bae0dcf2854b49e36932",
+    "url": "/greenthumb/static/media/illustration-home.b2c10a67.png"
+  },
+  {
+    "revision": "0df80ae3b5367929f7d2b52a7d29dec0",
+    "url": "/greenthumb/static/media/logo-greenthumb.0df80ae3.svg"
+  },
+  {
+    "revision": "3261490de046a3a96767a31ba10fc97e",
+    "url": "/greenthumb/static/media/toxic.3261490d.svg"
+  }
+]);
--- src/services/api.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({ baseURL: 'https://cors-anywhere.herokuapp.com/https://6nrr6n9l50.execute-api.us-east-1.amazonaws.com/default/front-plantTest-service' });
-
-export default api;
