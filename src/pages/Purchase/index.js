b4cc9aec12ef13b57bf1b7911599d2cb8e9d7be0
Bruno Souto <bruno.csouto@gmail.com>
2019-12-08 01:23:36 -0300

    Load data to purchase page

--- src/pages/Purchase/index.js
+++ src/pages/Purchase/index.js
@@ -1,12 +1,69 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { Component } from 'react';
+import api from '../../services/api';
+import './styles.css';
 
-const Purchase = () => {
-    let { id } = useParams();
-    console.log(id);
-    return (
-        <h1>Purchase</h1>
-    )
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/logo/logo-greenthumb.svg';
+
+
+
+export default class Purchase extends Component {
+    constructor(){
+        super();
+        this.state = {
+            id: null,
+            data: []
+        }
+    }
+
+    componentDidMount(){
+        document.body.style.backgroundColor = 'white';
+        const { id } = this.props.match.params;
+        this.setState({ id });
+        this.loadPlant({ id });
+    }
+    
+    loadPlant = async ({ id }) => {
+        await api.get(`plant?id=${id}`)
+            .then((res) => {
+                console.log(res)
+                const {data} = res;
+                this.setState({data});
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+    render(){
+        const {data} = this.state;
+        return (
+            <div className="container-purchase">
+                <div className="logo logo-purchase">
+                    <Link className="btn-home" to="/">
+                        <Logo className="logo-image" />
+                    </Link>
+                </div>
+                <div className="plant">
+                    <h1>{data.name}</h1>
+                    <h2>${data.price}</h2>
+                    <img src={data.url} alt={data.name} />
+                    <div className="specs">
+                        <div>{data.sun}</div>
+                        <div>{data.water}</div>
+                        <div>{data.toxicity}</div>
+                    </div>
+                </div>
+                <div className="form">
+                    <h1>Nice pick!</h1>
+                    <h2>Tell us your name and e-mail and we will get in touch about your order :)</h2>
+                    <p>Name</p>
+                    <input type="text" placeholder="Crazy Plant Person" />
+                    <p>E-mail</p>
+                    <input type="text" placeholder="plantperson@email.com" />
+                    <button>send</button>
+                </div>
+            </div>
+        )
+    }
 };
-
-export default Purchase;
